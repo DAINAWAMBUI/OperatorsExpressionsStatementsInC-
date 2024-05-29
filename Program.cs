f000f02442ba5ff7247771b86780385d1191a791
Daina Muiruri <61407935+DAINAWAMBUI@users.noreply.github.com>
2024-05-29 09:14:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace OperatorsExpressionsStatements
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //int a, b, c, d,x;
+            int x;
+
+            //assignment operators
+            int a = 2;
+            int b = 10;
+            int c = 21;
+            int d = 46;
+
+            //Additional Operators
+            x = 2 + 10;
+
+            //Subtraction Operator
+            x= 10 - 2;
+
+            //division Operator
+            x = 10 / 2;
+
+            //Multiplication Operator
+            x = 10 * 2;
+
+            //modulus operator
+            x = 10 % 2;
+
+            //Equality operator
+            if (c==21)
+            { }
+            //less than or equal to operator
+            if (c<=21)
+            { }
+            // greater than or equal to operator
+            if (d >=46)
+            { }
+            //Greater than operator
+            if (c > d)
+            { }
+            //Less that operator
+            if (d < c)
+            { }
+
+         
+
+            
+        }
+    }
+}
